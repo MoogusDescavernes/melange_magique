c150e69e37bca5df9326573754f5a907894b4172
Flavien Audin <flavien.audin@gmail.com>
2022-10-06 22:39:47 +0200

    Update version

--- mmassistant.user.js
+++ mmassistant.user.js
@@ -3,7 +3,7 @@
 // @namespace    Mountyhall
 // @description  Assistant Mélange Magique & Affichage % de stabilisation des compos
 // @author       Dabihul
-// @version      2.3.3.0
+// @version      2.3.3.1 (Hennet/Law) 
 // @include      */mountyhall/MH_Taniere/TanierePJ_o_Stock*
 // @include      */mountyhall/MH_Comptoirs/Comptoir_o_Stock*
 // @include      */mountyhall/MH_Follower/FO_Equipement*
