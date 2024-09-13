42812abf786f017e254aef9dfbdeed751c60812f
David-Github-DSO <dso.mail@pm.me>
2024-09-13 21:58:30 +0200

    Numéro de version +1

--- mmassistant.user.js
+++ mmassistant.user.js
@@ -3,7 +3,7 @@
 // @namespace    Mountyhall
 // @description  Assistant Mélange Magique & Affichage % de stabilisation des compos.
 // @author       Dabihul
-// @version      2.3.3.5
+// @version      2.3.3.6
 // @license      MIT
 // @include      */mountyhall/MH_Taniere/TanierePJ_o_Stock*
 // @include      */mountyhall/MH_Comptoirs/Comptoir_o_Stock*
